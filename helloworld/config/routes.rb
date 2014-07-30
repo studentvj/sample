e7576f239fe84015061a79133dfaeb24505c0041
vijayarajmahendran <vijayarajmahendran@gmail.com>
2014-07-30 02:40:18 -0700

    chnage1

--- helloworld/config/routes.rb
+++ helloworld/config/routes.rb
@@ -1,4 +1,5 @@
 Helloworld::Application.routes.draw do
+  get "test/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
